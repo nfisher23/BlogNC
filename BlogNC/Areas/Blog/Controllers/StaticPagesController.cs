1ad0974e0c415ecc9c80ddc656cd3420f52f28e0
Nick Fisher <fisher.nick14@gmail.com>
2018-04-02 15:06:50 -0700

    Base route established

---
+++ BlogNC/Areas/Blog/Controllers/StaticPagesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogNC.Areas.Blog.Controllers
+{
+    public class StaticPagesController : Controller
+    {
+        public ViewResult Index()
+        {
+            // temp placeholder to develop other functionality
+            return View();
+        }
+
+        public ViewResult FindStaticPage(string urlTitle)
+        {
+            // final product will look for user created static page entries
+            throw new NotImplementedException();
+        }
+    }
+}
--- BlogNC/Startup.cs
+++ BlogNC/Startup.cs
@@ -15,6 +15,7 @@ namespace BlogNC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,12 +23,18 @@ namespace BlogNC
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "",
+                    template: "{controller=StaticPages}/{action=Index}",
+                    defaults: new { area = "Blog" });
             });
         }
     }
